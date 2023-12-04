c471480e13366a1198d172faab8bad2063fd1c21
Tetsu Kurumisawa <tetsu.kurumisawa@yale.edu>
2023-12-04 15:15:57 -0500

    adding more data

--- data_processing/extract_per_player_help3.py
+++ data_processing/extract_per_player_help3.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
             data_list = [json.loads(line) for line in file]
             # Iterate through the data
             for entry in data_list:
-                if entry["id"] >= 229619:
+                if entry["id"] >= 231318 and entry["id"] < 236318:
                     continue_switch = True
                     flop = entry["board"][:3]
                     turn = entry["board"][:4]
